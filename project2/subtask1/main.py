4155aad1cf5ea1c811dac41da8e4b7ad87fb8a06
adityjha0 <aditya2005aps@gmail.com>
2024-03-14 21:40:10 +0530

    final version

--- project2/subtask1/main.py
+++ project2/subtask1/main.py
@@ -2,10 +2,10 @@ import os
 import moviepy.editor as mp
 import PyPDF2
 import sys
-import ffmpeg
 import whisper
 
-def extract_text_from_video(video_file, model='base'):
+# extect text from video
+def text_from_video(video_file, model='base'):
     whisper_model = whisper.load_model(model)
     video = mp.VideoFileClip(video_file)
     audio = video.audio
@@ -16,13 +16,15 @@ def extract_text_from_video(video_file, model='base'):
     os.remove(temp)
     return transcript
 
-def extract_text_from_audio(audio_file, model="base"):
+# extect text from audio
+def text_from_audio(audio_file, model="base"):
     whisper_model = whisper.load_model(model)    
     result = whisper_model.transcribe(audio_file)
-    transcript = result["text"]
+    transcript = result["text"] + '\n'
     return transcript
 
-def extract_text_from_pdf(pdf_file):
+# extect text from pdf
+def text_from_pdf(pdf_file):
     with open(pdf_file, 'rb') as file:
         pdf_reader = PyPDF2.PdfReader(file)
         num_pages = len(pdf_reader.pages)
@@ -32,7 +34,7 @@ def extract_text_from_pdf(pdf_file):
             text += page.extract_text() + '\n'
     return text
 
-
+# main function
 def main():
     print("Enter choice for file type : \n1. Video\n2. Audio\n3. Pdf\n?- ", end='')
     n = int(input())
@@ -44,26 +46,18 @@ def main():
     text = ''
     match n:
         case 1:
-            file = "test2.mp4"
-            text = extract_text_from_video(file)
+            text = text_from_video(file)
         case 2:
-            filepath , filename = os.path.split(file)
-            basename, ext = os.path.splitext(filename)
-            wav_file = f"{basename}.wav"
-            stream = ffmpeg.input(file)
-            stream = ffmpeg.output(stream, wav_file, acodec='pcm_s16le', ac=1, ar='16k')
-            ffmpeg.run(stream)
-            file = wav_file
-            text = extract_text_from_audio(file)
+            text = text_from_audio(file)
         case 3:
-            text = extract_text_from_pdf(file)
+            text = text_from_pdf(file)
         case _:
             sys.exit("ERROR")
 
     filepath , filename = os.path.split(file)
     basename, ext = os.path.splitext(filename)
 
-    output = f"{basename}.txt"
+    output = f"{filepath}/{basename}.txt"
 
     with open(output, 'w') as file:
         file.write(text)
